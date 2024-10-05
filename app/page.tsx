f15036b067a8c7dba697a432680205ecb6c1fcdd
Tuba Nafees <tuba.nafees4@gmail.com>
2024-10-05 20:09:39 +0500

    Assignment Completes

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 style={{ marginLeft: '70px' }}>
+      </h1>
+    <h1><b><br/> Hello World in Next JS </b></h1></div>
+      );
+}
+//This function is returning an html component.
+//Fragments or divs are recommended
