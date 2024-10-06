305686a78cd77e1d39c2f1f102bf3b3dba32668a
Tuba Nafees <tuba.nafees4@gmail.com>
2024-10-06 20:19:48 +0500

    Assignment Completes

---
+++ app/Components/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const About = () => {
+  return (
+    <section>
+      <h2>About Us</h2>
+      <p>This is a simple Next.js application demonstrating an alternate structure.</p>
+    </section>
+  );
+};
+export default About;
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
-import Image from "next/image";
+import React from 'react'; // Import React to resolve the UMD global error
+import Head from 'next/head';
+import Header from './components/Header';
+import About from './components/About';
 
 export default function Home() {
   return (
-    <div>
-      <h1 style={{ marginLeft: '70px' }}>
-      </h1>
-    <h1><b><br/> Hello World in Next JS </b></h1></div>
-      );
+    <>
+      <Head>
+        <title>My Next.js App</title>
+        <meta name="description" content="An alternative structure for Next.js components" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+      <About />
+    </>
+  );
 }
-//This function is returning an html component.
-//Fragments or divs are recommended
