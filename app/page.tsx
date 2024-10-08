a17b0488bf984edfc3ee5649fcc98da5856c16aa
Tuba Nafees <tuba.nafees4@gmail.com>
2024-10-08 15:10:23 +0500

    Assignment Updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-export default function starttask(){
-  return
-  <div>
-    <h1>Hello World</h1>
-  </div>
+export default function starttask() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
 }
