6f8b48f20df2dd0b70cb31c66fcb953cdd371a8b
Tuba Nafees <tuba.nafees4@gmail.com>
2024-10-08 14:48:22 +0500

    Assignment Modifies

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,5 @@
-import React from 'react'; // Import React to resolve the UMD global error
-import Head from 'next/head';
-import Header from './components/Header';
-import About from './components/About';
-
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>My Next.js App</title>
-        <meta name="description" content="An alternative structure for Next.js components" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <Header />
-      <About />
-    </>
-  );
+export default function(){
+  <div>
+    <h1>Hello World</h1>
+  </div>
 }
